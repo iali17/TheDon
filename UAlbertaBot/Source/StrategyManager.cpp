18ff543ac11fb0610eeb915900707d89041c7713
Brosda <abrosda@ualberta.ca>
2016-11-15 20:42:49 -0700

    Less marines more killing kids

--- UAlbertaBot/Source/StrategyManager.cpp
+++ UAlbertaBot/Source/StrategyManager.cpp
@@ -228,11 +228,13 @@ const MetaPairVector StrategyManager::getTerranBuildOrderGoal() const
 	//Added by Alex Nov 14
 	else if (Config::Strategy::StrategyName == "Terran_MarineDrop")
 	{
-		goal.push_back(std::pair<MetaType, int>(BWAPI::UnitTypes::Terran_Marine, numMarines + 55));
+		goal.push_back(std::pair<MetaType, int>(BWAPI::UnitTypes::Terran_Marine, numMarines + 8));
 
 		if (numMarines > 6) {
-			goal.push_back(std::pair<MetaType, int>(BWAPI::UnitTypes::Terran_Engineering_Bay, 1));
 			goal.push_back(std::pair<MetaType, int>(BWAPI::UnitTypes::Terran_Medic, numMedics + 12));
+		}
+		if (numBay < 1) {
+			goal.push_back(std::pair<MetaType, int>(BWAPI::UnitTypes::Terran_Engineering_Bay, 1));
 		}
 		if (numMedics > 2) {
 			goal.push_back(std::pair<MetaType, int>(BWAPI::UnitTypes::Terran_Dropship, 4));

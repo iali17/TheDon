95f3fffafd1d697a7d1417bc56adda4a7480f135
hnkhan <hnkhan@ualberta.ca>
2016-11-15 21:39:16 -0700

    Sorry Alex
    Test

--- UAlbertaBot/Source/Config.cpp
+++ UAlbertaBot/Source/Config.cpp
@@ -7,7 +7,7 @@ namespace Config
     {
         bool ConfigFileFound                = false;
         bool ConfigFileParsed               = false;
-        std::string ConfigFileLocation      = "bwapi-data/AI/UAlbertaBot_Config.txt";
+        std::string ConfigFileLocation      = "C:/Users/Hussain/Desktop/git/TheDon/UAlbertaBot/UAlbertaBot_Config.txt";
     }
 
     namespace Strategy
